690f2d07d7eabc008ee2e1dbd2537c6cb60d2567
dangduc2604 <mduc2604@gmail.com>
2026-06-17 08:38:51 +0700

    feat(routes): map GET /users endpoint to controller

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 
 // Các route quản trị sẽ viết ở đây
-
+router.get('/users', adminController.getAllUsers);
 module.exports = router;
