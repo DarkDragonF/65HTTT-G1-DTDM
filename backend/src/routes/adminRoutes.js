e5a56557212e49fe525efec6a4b981f14d351ab2
dangduc2604 <mduc2604@gmail.com>
2026-06-17 08:40:51 +0700

    feat(routes): map PUT /users/:id/status endpoint

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -4,4 +4,5 @@ const adminController = require('../controllers/adminController');
 
 // Các route quản trị sẽ viết ở đây
 router.get('/users', adminController.getAllUsers);
+router.put('/users/:id/status', adminController.changeUserStatus);
 module.exports = router;
