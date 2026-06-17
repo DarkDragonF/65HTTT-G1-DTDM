c6f393cfc5fbcb7c7167d4338d1a8aec110a3459
dangduc2604 <mduc2604@gmail.com>
2026-06-17 08:32:33 +0700

    chore: setup initial router framework in adminRoutes

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const adminController = require('../controllers/adminController');
+
+// Các route quản trị sẽ viết ở đây
+
 module.exports = router;
