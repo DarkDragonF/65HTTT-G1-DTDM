c87d9e61b580d6d78d5fe081599088ef610d3ac3
hieunguyenvan2855 <nguyenvanhieu28052005@gmail.com>
2026-06-22 22:23:28 +0700

    refactor: Tối ưu hóa code và dọn dẹp các warnings trước khi review

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
-import Canteens from './pages/Canteens'; // Import trang Canteens
+import Canteens from './pages/Canteens';
 import CanteenDetail from './pages/CanteenDetail';
+import MenuManager from './pages/MenuManager'; // 1. Import trang quản lý
 
 function App() {
   return (
@@ -10,8 +11,11 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
-          <Route path="canteens" element={<Canteens />} /> {/* Thêm route này */}
-          <Route path="canteens/:id" element={<CanteenDetail />} /> {/* Thêm dòng này */}
+          <Route path="canteens" element={<Canteens />} />
+          <Route path="canteens/:id" element={<CanteenDetail />} />
+          
+          {/* 2. Thêm route quản lý thực đơn */}
+          <Route path="manage-menu" element={<MenuManager />} /> 
         </Route>
       </Routes>
     </BrowserRouter>
